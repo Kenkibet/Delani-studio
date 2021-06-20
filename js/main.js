0905fa22b7777f61781396110d4c073bc633fd08
ken kibet <ken.kibet@student.moringaschool.com>
2021-06-21 00:57:05 +0300

    Switched to arrow functions

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
-$(document).ready(function () {
+$(document).ready(()=>{
     // first, hide all the content under the logos
     $(".designContent").hide();
     $(".development-content").hide();
     $(".product-content").hide();
 
     // Add click listeners
-    $("#design").click(function () {
+    $("#design").click(()=>{
         $("#design").hide();
         $(".designContent").show();
     });
-    $(".designContent").click(function () {
+    $(".designContent").click(()=>{
         $(".designContent").hide();
         $("#design").show();
     });
 
-    $("#development").click(function () {
+    $("#development").click(()=>{
         $("#development").hide();
         $(".development-content").show();
     });
-    $(".development-content").click(function () {
+    $(".development-content").click(()=>{
         $(".development-content").hide();
         $("#development").show();
     });
 
-    $("#product").click(function () {
+    $("#product").click(()=>{
         $("#product").hide();
         $(".product-content").show();
     });
-    $(".product-content").click(function () {
+    $(".product-content").click(()=>{
         $(".product-content").hide();
         $("#product").show();
     });
